c92d6419ca7de412da63129bc94e20e131198263
Alejandro Ramírez Gómez <alrg1720@gmail.com>
2021-01-02 18:57:22 -0600

    Menu hambuger terminado
    Menu hamburger terminado con efecto de difuminado, se implementa acciones
    con JS para abrir y cerrar el menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
+const show         = document.getElementById('show-menu')
+const menu         = document.getElementById('main-menu')
+const menuList     = document.getElementById('main-menu__list')
+const showInactive = document.getElementById('close-menu')
+// const close        = document.getElementById('')
+    show.addEventListener('click',()=>{
+            menu.classList.add('main-menu')
+            menuList.classList.toggle('show-menu')
+            show.style.display = "none";
+            showInactive.classList.toggle('close-menu-active')
+            document.body.style.overflowY = "hidden";
+            
+    });
+
+    showInactive.addEventListener('click', () =>{
+        menu.classList.remove('main-menu')
+        menuList.classList.toggle('show-menu')
+        show.style.display = "block";
+        showInactive.classList.toggle('close-menu-active')
+        document.body.style.overflowY = "scroll";
+
+});
 
